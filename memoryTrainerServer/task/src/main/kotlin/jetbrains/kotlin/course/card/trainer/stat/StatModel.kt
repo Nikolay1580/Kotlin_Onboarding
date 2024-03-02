8d2b9f8012ab7f567dbd7533bb5d5af786b952a2
Nik Tsonev <nikolay.l.tsonev@gmail.com>
2024-03-02 10:06:48 +0100

    Back up in case it all breaks due to AliasServer

---
+++ memoryTrainerServer/task/src/main/kotlin/jetbrains/kotlin/course/card/trainer/stat/StatModel.kt
@@ -0,0 +1,6 @@
+package jetbrains.kotlin.course.card.trainer.stat
+import jetbrains.kotlin.course.card.trainer.card.Back
+
+data class Stat(val knownBacks: List<Back>, val unknownBacks: List<Back>)
+
+
